20b845afe3a00f64d3c6c2e2f81613b7b6c056c6
root <tony.formica@yale.edu>
2020-03-14 12:48:39 -0400

    Maybe

--- ARP2.py
+++ ARP2.py
@@ -39,10 +39,10 @@ for i in sources:
 
     stopwords.extend(newstpwrds)
 
-    with open(f'ZAR1Dummy_{today}.txt', 'a') as fo:
+    with open(f'ZAR2Dummy_{today}.txt', 'a') as fo:
         fo.write(text)
 
-    with open(f'ZAR1Dummy_{today}.txt', 'r', errors='ignore') as fo1:
+    with open(f'ZAR2Dummy_{today}.txt', 'r', errors='ignore') as fo1:
         csvWriter = csv.writer(fo1)
         msm = fo1.readlines()
 
@@ -51,7 +51,9 @@ for i in sources:
         tokenized_var = word_tokenize(i)
         for word in tokenized_var:
             if not word in stopwords and "mediamatters" not in word\
-                    and ".com" not in word and "<" not in word:
+                    and ".com" not in word and "<" not in word\
+                    and "html" not in word and "storybody" not in word\
+                    and "t.coe" not in word and "data" not in word:
                 clean.append(str(word))
 
     clean1 = str(clean)
@@ -59,11 +61,11 @@ for i in sources:
     add = [i for i in clean1 if i.isalpha() or i.isspace() or i in period]
     add1 = "".join(add)
 
-    with open(f'Print_AR1_{today}.txt', 'a') as fo2:
+    with open(f'Print_AR2_{today}.txt', 'a') as fo2:
         fo2.write(add1)
 
     for i in clean:
-        with open(f'Print_AR1_{today}.csv', 'a') as fo3:
+        with open(f'Print_AR2_{today}.csv', 'a') as fo3:
             csvWriter = csv.writer(fo3)
             analysis = TextBlob(i)
             csvWriter.writerow([i, analysis.polarity, analysis.subjectivity])  
